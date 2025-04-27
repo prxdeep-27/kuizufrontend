a8c973a3fb6cb75b1a9ac3d50644c431847a861d
prxdeep-27 <pradeepmanikandann@gmail.com>
2025-04-27 15:00:02 +0530

    Pushed only backend, routes, and models

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,7 @@ const PORT = process.env.PORT || 3004;
 connectDB(); // connect to Atlas
 
 // Middleware
-app.use(cors({
-  origin: "http://localhost:3000"
-}));
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
