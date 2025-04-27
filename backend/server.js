12e25d225258e8afce844645ef32c42572c6a754
prxdeep-27 <pradeepmanikandann@gmail.com>
2025-04-27 12:25:42 +0530

    Fixed authRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,4 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// require('dotenv').config();
 
-// const authRoutes = require('../routes/authroutes');
-
-// const app = express();
-// const PORT = process.env.PORT || 3005;
-
-// app.use(cors());
-// app.use(express.json());
-
-// // MongoDB Connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => console.log("MongoDB  connected"))
-//   .catch(err => console.error("MongoDB error:", err));
-
-// // Routes
-// app.use('/api/auth', authRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 
 const express = require('express');
